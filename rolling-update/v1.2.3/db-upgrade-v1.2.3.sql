d455b7dd9ac2797ba264e94525fd2dc9a2f8ce9d
kuizhang <kuizhang@harmonycloud.cn>
2017-11-07 10:40:35 +0800

    修改websphere端口
    Change-Id: Id9cae766237695089662b30431d8f6f75974440b

--- rolling-update/v1.2.3/db-upgrade-v1.2.3.sql
+++ rolling-update/v1.2.3/db-upgrade-v1.2.3.sql
@@ -1,11 +1,11 @@
 --添加字段：区分应用模板公私有
 ALTER TABLE `k8s_auth_server`.`business_templates` 
 ADD COLUMN `is_public` TINYINT NULL DEFAULT 0 AFTER `image_list`;
---添加字段：区分服务模板公私有
+--添加字段：区分服务模板公私有9
 ALTER TABLE `k8s_auth_server`.`service_templates` 
 ADD COLUMN `is_public` TINYINT NULL DEFAULT 0 AFTER `image_list`;
 --修改websphere模板
-update k8s_auth_server.service_templates set deployment_content='[{\"annotation\":\"\",\"clusterIP\":\"\",\"containers\":[{\"args\":[],\"command\":[],\"configmap\":[],\"env\":[],\"img\":\"onlineshop/websphere-traditional\",\"livenessProbe\":null,\"log\":\"\",\"name\":\"websphere\",\"ports\":[{\"containerPort\":\"\",\"expose\":\"true\",\"port\":\"30300\",\"protocol\":\"TCP\"},{\"containerPort\":\"\",\"expose\":\"true\",\"port\":\"9080\",\"protocol\":\"TCP\"}],\"readinessProbe\":null,\"resource\":{\"cpu\":\"1000m\",\"memory\":\"1024\"},\"securityContext\":{\"add\":[],\"drop\":[],\"privileged\":false,\"security\":false},\"storage\":[],\"tag\":\"8.5.5.9-install\"}],\"hostIPC\":false,\"hostName\":\"\",\"hostPID\":false,\"instance\":\"1\",\"labels\":\"\",\"logPath\":\"\",\"logService\":\"\",\"name\":\"websphere\",\"namespace\":\"garydemo-garyns\",\"nodeSelector\":\"HarmonyCloud_Status=C\",\"restartPolicy\":\"Always\",\"sessionAffinity\":\"\"}]' where id=10 and name='websphere'
+update k8s_auth_server.service_templates set deployment_content='[{\"annotation\":\"\",\"clusterIP\":\"\",\"containers\":[{\"args\":[],\"command\":[],\"configmap\":[],\"env\":[],\"img\":\"onlineshop/websphere-traditional\",\"livenessProbe\":null,\"log\":\"\",\"name\":\"websphere\",\"ports\":[{\"containerPort\":\"\",\"expose\":\"true\",\"port\":\"9043\",\"protocol\":\"TCP\"},{\"containerPort\":\"\",\"expose\":\"true\",\"port\":\"9080\",\"protocol\":\"TCP\"}],\"readinessProbe\":null,\"resource\":{\"cpu\":\"1000m\",\"memory\":\"1024\"},\"securityContext\":{\"add\":[],\"drop\":[],\"privileged\":false,\"security\":false},\"storage\":[],\"tag\":\"8.5.5.9-install\"}],\"hostIPC\":false,\"hostName\":\"\",\"hostPID\":false,\"instance\":\"1\",\"labels\":\"\",\"logPath\":\"\",\"logService\":\"\",\"name\":\"websphere\",\"namespace\":\"garydemo-garyns\",\"nodeSelector\":\"HarmonyCloud_Status=C\",\"restartPolicy\":\"Always\",\"sessionAffinity\":\"\"}]' where id>0 and name='websphere';
 
 
 --OAM 告警表结构更改：
