4c42157b4ab74a2d0ab78edfd85dd558b6675153
zhangkui <kuizhang@harmonycloud.cn>
2018-07-07 13:22:07 +0800

    自动伸缩参数校验错误修改
    Change-Id: If00a8698b31805add8f23471d0b3f92d2ba57c71

--- k8s-mars-api/src/main/java/com/harmonycloud/api/application/ComplexAutoscaleController.java
+++ k8s-mars-api/src/main/java/com/harmonycloud/api/application/ComplexAutoscaleController.java
@@ -119,7 +119,9 @@ public class ComplexAutoscaleController {
         if(autoScale == null){
             return MessageUtil.getMessage(ErrorCodeMessage.PARAMETER_VALUE_NOT_PROVIDE);
         }
-        if(autoScale.getTargetCpuUsage() == null && autoScale.getTargetTps() == null
+        if(autoScale.getTargetCpuUsage() == null
+                && autoScale.getTargetMemoryUsage() == null
+                && autoScale.getTargetTps() == null
                 && CollectionUtils.isEmpty(autoScale.getTimeMetricScales())
                 && CollectionUtils.isEmpty(autoScale.getCustomMetricScales())){
             return MessageUtil.getMessage(ErrorCodeMessage.INDICATOR);
