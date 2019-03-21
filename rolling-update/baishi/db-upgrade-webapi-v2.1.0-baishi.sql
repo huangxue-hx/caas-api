2a7fe8bac93d438dfd4373c6bea87e37ada81ebf
zhangkui <kuizhang@harmonycloud.cn>
2019-03-21 15:32:34 +0800

    更新百世升级步骤

--- rolling-update/baishi/db-upgrade-webapi-v2.1.0-baishi.sql
+++ rolling-update/baishi/db-upgrade-webapi-v2.1.0-baishi.sql
@@ -6,30 +6,31 @@ VALUES ('/clusters/label/nodes', 'system', 'system');
 INSERT INTO `k8s_auth_server`.`url_dic` (`url`, `module`, `resource`)
 VALUES ('/clusters/nodes/groups', 'infrastructure', 'clustermgr');
 
-INSERT INTO `k8s_auth_server`.`resource_menu_role` (`weight`, `create_time`, `update_time`, `available`, `role_id`, `rmid`)
-VALUES ('41', '2019-01-14 17:28:00', '2019-01-14 17:28:00', '1', '1', '41');
-
 INSERT INTO `k8s_auth_server`.`url_dic` (`url`, `module`, `resource`) VALUES ('/system/api-server/auditlogs', 'log', 'auditlog');
 INSERT INTO `k8s_auth_server`.`url_dic` (`url`, `module`, `resource`) VALUES ('/system/api-server/auditlogs/count', 'log', 'auditlog');
 INSERT INTO `k8s_auth_server`.`url_dic` (`url`, `module`, `resource`) VALUES ('/system/api-server/auditlogs/namespaces', 'log', 'auditlog');
 
-INSERT INTO `k8s_auth_server`.`resource_menu` (`name`, `name_en`, `type`, `url`, `weight`, `create_time`, `update_time`, `available`, `icon_name`, `isparent`, `parent_rmid`, `module`)
-VALUES ('集群审计', 'k8s Audit', 'menu', 'k8sAuditLog', '41', '2019-01-14 16:47:00', '2019-01-14 16:50:56', '1', '', '0', '9', 'k8sauditlog');
-
 INSERT INTO `k8s_auth_server`.`url_dic` (`url`, `module`, `resource`) VALUES ('/tenants/*/projects/*/deploys/monitor', 'system', 'system');
-
-INSERT INTO `k8s_auth_server`.`resource_menu` (`name`, `name_en`, `type`, `url`, `weight`, `create_time`, `update_time`, `available`, `icon_name`, `isparent`, `parent_rmid`, `module`)
-VALUES ('监控中心', 'Monitor Center', 'menu', 'monitorCenter', '42', '2019-02-14 16:47:00', '2019-02-14 16:50:56', '1', '', '1', '0', 'monitorcenter');
-
-INSERT INTO `k8s_auth_server`.`resource_menu_role` (`weight`, `create_time`, `update_time`, `available`, `role_id`, `rmid`)
-VALUES ('42', '2019-01-14 17:28:00', '2019-01-14 17:28:00', '1', '1', '42');
-
-INSERT INTO `k8s_auth_server`.`resource_menu_role` (`weight`, `create_time`, `update_time`, `available`, `role_id`, `rmid`)
-VALUES ('42', '2019-01-14 17:28:00', '2019-01-14 17:28:00', '1', '2', '42');
 
 INSERT INTO `k8s_auth_server`.`url_dic`(`url`, `module`, `resource`) VALUES ('/tenants/*/projects/*/ippools', 'tenant', 'ippoolsmgr');
 INSERT INTO `k8s_auth_server`.`url_dic`(`url`, `module`, `resource`) VALUES ('/tenants/*/projects/*/ippools/checkcluster', 'tenant', 'ippoolsmgr');
 INSERT INTO `k8s_auth_server`.`url_dic`(`url`, `module`, `resource`) VALUES ('/tenants/*/clusters', 'tenant', 'basic');
+
+
+INSERT INTO `resource_menu` (`id`,`name`,`name_en`,`type`,`url`,`weight`,`create_time`,`update_time`,`available`,`icon_name`,`isparent`,`parent_rmid`,`module`)
+VALUES (33,'集群审计','k8s Audit','menu','k8sAuditLog',33,'2019-01-14 16:47:00','2019-01-14 16:50:56',1,'',0,9,'k8sauditlog');
+
+INSERT INTO `resource_menu_role` (`weight`,`create_time`,`update_time`,`available`,`role_id`,`rmid`)
+VALUES (33,'2019-01-14 17:28:00','2019-01-14 17:28:00',1,1,33);
+
+INSERT INTO `resource_menu` (`id`,`name`,`name_en`,`type`,`url`,`weight`,`create_time`,`update_time`,`available`,`icon_name`,`isparent`,`parent_rmid`,`module`)
+VALUES (34,'监控中心','Monitor Center','menu','monitorCenter',34,'2019-02-14 16:47:00','2019-02-14 16:50:56',1,'menu-icon mi-storage',1,0,'monitorcenter');
+
+INSERT INTO `resource_menu_role` (`weight`,`create_time`,`update_time`,`available`,`role_id`,`rmid`)
+VALUES (34,'2019-01-14 17:28:00','2019-01-14 17:28:00',1,1,34);
+
+INSERT INTO `resource_menu_role` (`weight`,`create_time`,`update_time`,`available`,`role_id`,`rmid`)
+VALUES (34,'2019-01-14 17:28:00','2019-01-14 17:28:00',1,2,34);
 
 -- ----------------------------
 -- Table structure for project_ip_pool
