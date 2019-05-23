b3070cc3d5c4a42d211c2ac298aef15f65d8df59
zhangkui <kuizhang@harmonycloud.cn>
2019-05-23 13:53:14 +0800

    脚本更新

--- rolling-update/2.3.2/db-upgrade-webapi-v2.3.2.sql
+++ rolling-update/2.3.2/db-upgrade-webapi-v2.3.2.sql
@@ -1,8 +1,5 @@
 use k8s_auth_server;
-
-SET NAMES utf8;
-SET FOREIGN_KEY_CHECKS = 0;
-
+-- debug功能
 -- ----------------------------
 --  Table structure for `debug_state`
 -- ----------------------------
@@ -18,15 +15,6 @@ CREATE TABLE `debug_state` (
   `port` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
-
--- ----------------------------
---  Records of `debug_state`
--- ----------------------------
-BEGIN;
-INSERT INTO `debug_state` VALUES ('1', 'admin', 'stop', '2019-05-18 07:25:34', null, 'debug-test', 'aaa', null), ('2', 'dddyl', 'stop', '2019-05-18 08:18:16', null, 'debug-test', 'rrr', '8081');
-COMMIT;
-
-SET FOREIGN_KEY_CHECKS = 1;
 
 
 insert into `k8s_auth_server`.`url_dic` ( `module`, `url`, `resource`) values ( 'namespace', '/users/namespace/username/*', 'namespace');
@@ -46,5 +34,3 @@ alter table user add `crowd_user_id` int(11) DEFAULT NULL;
 
 INSERT INTO `k8s_auth_server`.`url_dic` (`url`, `module`, `resource`) VALUES ('/users/sync', 'system', 'system');
 insert into k8s_auth_server.url_dic(url,module,resource) values('/system/configs/crowd','system','system');
-
--- debug功能
